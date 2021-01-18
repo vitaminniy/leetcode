02de812aefe9fd8c5f9069663afd61acd69c0df0
Udalov Max <re.udalov@gmail.com>
2021-01-18 22:10:28 +0300

    add swap_nodes_in_pairs

---
+++ swap_nodes_in_pairs/README.md
@@ -0,0 +1,4 @@
+# Swap Nodes in Pairs
+
+https://leetcode.com/problems/swap-nodes-in-pairs/
+
---
+++ swap_nodes_in_pairs/solution.cpp
@@ -0,0 +1,24 @@
+struct ListNode {
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+
+  int val;
+  ListNode *next;
+};
+
+class Solution {
+public:
+  ListNode *swapPairs(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
+    }
+
+    auto nextpair = swapPairs(head->next->next);
+    auto next = head->next;
+    head->next = nextpair;
+    next->next = head;
+
+    return next;
+  }
+};
