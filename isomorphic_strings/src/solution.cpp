69831c6ce5fd5d4c0e89176b9c33450e3e05b8eb
Udalov Max <re.udalov@gmail.com>
2021-07-13 01:27:23 +0300

    isomorphic_strings: speed up with vectors instead of unordered_maps

--- isomorphic_strings/src/solution.cpp
+++ isomorphic_strings/src/solution.cpp
@@ -1,41 +1,24 @@
 #include <string>
-#include <unordered_map>
+#include <vector>
 
 class Solution {
 public:
   bool isIsomorphic(const std::string &source, const std::string &target) {
-    std::unordered_map<char, char> seen;
-    std::unordered_map<char, char> reverse;
+    if (source.size() != target.size()) {
+      return false;
+    }
 
-    seen.reserve(source.size());
-    seen.reserve(target.size());
+    std::vector<char> source_chars(256, 0);
+    std::vector<char> target_chars(256, 0);
 
-    for (auto source_it = source.begin(), target_it = target.begin();
-         source_it != source.end() && target_it != target.end();
-         source_it++, target_it++) {
-      const auto s = *source_it;
-      const auto t = *target_it;
-
-      const auto seen_it = seen.find(s);
-      const auto reverse_it = reverse.find(t);
-
-      if (seen_it == seen.end() && reverse_it == reverse.end()) {
-        // new mapping for both source and target
-        seen[s] = t;
-        reverse[t] = s;
-        continue;
-      }
-
-      if ((seen_it != seen.end() && reverse_it == reverse.end()) ||
-          (seen_it == seen.end() && reverse_it != reverse.end())) {
-        // no two characters may map to the same character
+    for (size_t i = 0; i < source.size(); ++i) {
+      // wrong mapping
+      if (source_chars[source[i]] != target_chars[target[i]]) {
         return false;
       }
 
-      if (seen_it->second != reverse_it->first ||
-          seen_it->first != reverse_it->second) {
-        return false;
-      }
+      source_chars[source[i]] = target[i];
+      target_chars[target[i]] = target[i];
     }
 
     return true;
