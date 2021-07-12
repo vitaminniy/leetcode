60e9496df01c756df8636c62d934aab5758fafde
Udalov Max <re.udalov@gmail.com>
2021-07-13 00:53:13 +0300

    add clang-format command

---
+++ cmake/FindClangFormat.cmake
@@ -0,0 +1,35 @@
+# Find clang-format
+
+set(CLANG_FORMAT_EXCLUDE_PATTERNS  "build/" "lib/" ${CMAKE_BINARY_DIR})
+
+if(NOT CLANG_FORMAT_BIN_NAME)
+    set(CLANG_FORMAT_BIN_NAME clang-format)
+endif()
+
+# if custom path check there first
+if(CLANG_FORMAT_ROOT_DIR)
+    find_program(CLANG_FORMAT_BIN
+        NAMES
+        ${CLANG_FORMAT_BIN_NAME}
+        PATHS
+        "${CLANG_FORMAT_ROOT_DIR}"
+        NO_DEFAULT_PATH)
+endif()
+
+find_program(CLANG_FORMAT_BIN NAMES ${CLANG_FORMAT_BIN_NAME})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    ClangFormat
+    DEFAULT_MSG
+    CLANG_FORMAT_BIN)
+
+mark_as_advanced(CLANG_FORMAT_BIN)
+
+if(ClangFormat_FOUND)
+    # A CMake script to find all source files and setup clang-format targets
+    # for them
+    include(clang-format)
+else()
+    message("clang-format not found. Not setting up format targets")
+endif()
