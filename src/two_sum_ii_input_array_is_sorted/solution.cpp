203f64813c8100539047419348544e2909248fa5
Udalov Max <re.udalov@gmail.com>
2022-04-02 15:29:27 +0300

    two_sum_ii: changed solution to 2 pointers approach

--- src/two_sum_ii_input_array_is_sorted/solution.cpp
+++ src/two_sum_ii_input_array_is_sorted/solution.cpp
@@ -3,31 +3,21 @@
 class Solution {
  public:
   std::vector<int> twoSum(const std::vector<int>& numbers, int target) {
-    const int size = numbers.size();
-    for (int i = 0; i < size; i++) {
-      auto diff = target - numbers[i];
-      if (diff < numbers[i]) {
-        // array is sorted in non-decreasing order and there are no elements
-        // that are less than current.
-        return {};
+    int left = 0;
+    int right = numbers.size() - 1;
+    while (left <= right) {
+      auto sum = numbers[left] + numbers[right];
+      if (sum == target) {
+        return {++left, ++right};
       }
 
-      int left = i + 1;
-      int right = size - 1;
-      while (left <= right) {
-        const auto mid = left + ((right - left) / 2);
-        if (numbers[mid] == diff) {
-          return {i + 1, mid + 1};
-        }
-
-        if (numbers[mid] > diff) {
-          right = mid - 1;
-        } else {
-          left = mid + 1;
-        }
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
       }
     }
 
-    return {};
+    return numbers;
   }
 };
