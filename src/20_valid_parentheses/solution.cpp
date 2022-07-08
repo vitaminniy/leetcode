33aa942ba83bae29c6259108d259f94923d88cf5
Udalov Max <re.udalov@gmail.com>
2022-07-08 21:33:43 +0300

    20 (parnetheses): speedup

--- src/20_valid_parentheses/solution.cpp
+++ src/20_valid_parentheses/solution.cpp
@@ -6,41 +6,32 @@ class Solution {
   bool isValid(const std::string& s) const {
     std::vector<char> stack;
     for (const auto c : s) {
-      switch (c) {
-        case '(':
-        case '[':
-        case '{':
-          stack.push_back(c);
-          break;
-        case ')':
-        case ']':
-        case '}': {
-          if (stack.empty()) {
-            return false;
-          }
-
-          auto opener = GetMatching(c);
-          if (opener != stack.back()) {
-            return false;
-          }
-          stack.pop_back();
-          break;
-        }
+      if (c == '(' || c == '[' || c == '{') {
+        stack.push_back(c);
+        continue;
       }
+
+      if (stack.empty()) {
+        return false;
+      }
+
+      const auto opener = GetMatching(c);
+      if (opener != stack.back()) {
+        return false;
+      }
+      stack.pop_back();
     }
     return stack.empty();
   }
 
  private:
   static char GetMatching(char c) {
-    switch (c) {
-      case ')':
-        return '(';
-      case ']':
-        return '[';
-      case '}':
-        return '{';
+    if (c == ')') {
+      return '(';
     }
-    throw std::runtime_error("invalid char");
+    if (c == ']') {
+      return '[';
+    }
+    return '{';
   }
 };
