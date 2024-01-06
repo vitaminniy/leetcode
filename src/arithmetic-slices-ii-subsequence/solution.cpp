96195f3f3749f07073e8f9c82e8c38fda5aac611
Max Udalov <re.udalov@gmail.com>
2024-01-06 17:23:57 +0400

    src/arithmetic-slices-ii: fix gcc build

--- src/arithmetic-slices-ii-subsequence/solution.cpp
+++ src/arithmetic-slices-ii-subsequence/solution.cpp
@@ -5,13 +5,12 @@ class Solution {
  public:
   int numberOfArithmeticSlices(std::vector<int>& nums) {
     const int size = nums.size();
-    std::int64_t result = 0;
-    std::vector<std::map<std::int64_t, int>> counter(size);
+    int result = 0;
+    std::vector<std::map<int, int>> counter(size);
 
     for (int i = 1; i < size; i++) {
       for (int j = 0; j < i; j++) {
-        auto delta = static_cast<std::int64_t>(nums[i]) -
-                     static_cast<std::int64_t>(nums[j]);
+        auto delta = nums[i] - nums[j];
         auto sum = 0;
 
         if (auto it = counter[j].find(delta); it != counter[j].end()) {
@@ -23,6 +22,6 @@ class Solution {
       }
     }
 
-    return static_cast<int>(result);
+    return result;
   }
 };
