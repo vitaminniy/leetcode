9f32e7845850930487b1d416caaecc85e7135833
Udalov Max <re.udalov@gmail.com>
2025-05-18 11:13:29 +0400

    cmd: add 144 binary tree preorder traversal

---
+++ cmd/144_bintree_preorder/solution.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+struct TreeNode {
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+
+  int val{0};
+  TreeNode* left{nullptr};
+  TreeNode* right{nullptr};
+};
+
+class Solution {
+ public:
+  std::vector<int> preorderTraversal(const TreeNode* root) const {
+    std::vector<int> result;
+
+    traverse(root, result);
+
+    return result;
+  }
+
+ private:
+  void traverse(const TreeNode* node, std::vector<int>& acc) const {
+    if (node == nullptr) {
+      return;
+    }
+
+    acc.push_back(node->val);
+    traverse(node->left, acc);
+    traverse(node->right, acc);
+  }
+};
