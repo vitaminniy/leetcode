c8c5c88c93f13a090b06bf045a3f9b6a88b75189
Udalov Max <re.udalov@gmail.com>
2025-06-05 15:13:48 +0400

    cmd: add 1249 min removes to make valid parentheses

---
+++ cmd/1249_min_remove_to_make_valid_pars/solution.cpp
@@ -0,0 +1,37 @@
+#include <string>
+
+class Solution {
+public:
+    std::string minRemoveToMakeValid(std::string& s) const {
+        int counter = 0;
+
+        for (std::size_t i = 0; i < s.size(); i++) {
+            if (s[i] == '(') { counter++; }
+            if (s[i] == ')') { 
+                if (counter == 0) {
+                    s[i] = '*';
+                } else {
+                    counter--;
+                }
+            }
+        }
+        
+        for (int i = static_cast<int>(s.size()) - 1; i >= 0; i--) {
+            if (counter > 0 && s[i] == '(')  {
+                s[i] = '*';
+                counter--;
+            }
+        }
+
+        std::size_t i = 0;
+        for (std::size_t j = 0; j < s.size(); j++) {
+            if (s[j] != '*') {
+                s[i++] = s[j];
+            }
+        }
+
+        s.resize(i);
+
+        return s;
+    }
+};
