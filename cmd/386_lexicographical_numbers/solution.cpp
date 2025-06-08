1663dc40347d9b6269cbb5e8468df34795a01b59
Udalov Max <re.udalov@gmail.com>
2025-06-08 12:20:53 +0400

    cmd/386: make more loopy

--- cmd/386_lexicographical_numbers/solution.cpp
+++ cmd/386_lexicographical_numbers/solution.cpp
@@ -7,46 +7,25 @@ class Solution {
       return {};
     }
 
-    const auto digits = countDigits(n);
-
     std::vector<int> result;
     result.reserve(n);
 
-    dfs(result, 1, n, digits);
+    for (auto i = 1; i <= 9; i++) {
+      dfs(result, i, n);
+    }
 
     return result;
   }
 
  private:
-  void dfs(std::vector<int>& carry, int current, int n, int ndigits) const {
+  void dfs(std::vector<int>& carry, int current, int n) const {
     if (current > n) {
       return;
     }
 
     carry.push_back(current);
-
-    if (ndigits > 1) {
-      dfs(carry, current * 10, n, ndigits - 1);
+    for (auto i = 0; i <= 9; i++) {
+      dfs(carry, (current * 10) + i, n);
     }
-
-    if (current % 10 == 9) {
-      return;
-    }
-
-    dfs(carry, current + 1, n, ndigits);
-  }
-
-  int countDigits(int n) const {
-    if (n == 0) {
-      return 1;
-    }
-
-    int result = 0;
-    while (n > 0) {
-      n = n / 10;
-      result++;
-    }
-
-    return result;
   }
 };
